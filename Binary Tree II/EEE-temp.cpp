6d3e91df420c7d48c14c18960be1da16d3d69b8f
hellozts4120 <14307110190@fudan.edu.cn>
2015-09-25 16:53:47 +0800

    need debugging.

--- Binary Tree II/EEE-temp.cpp
+++ Binary Tree II/EEE-temp.cpp
@@ -7,18 +7,22 @@ using namespace std;
 
 int variable;
 int n;
+int result;
 char dispbuf[100][100];
 char str[51];
+int numSeq[51];
 char OutStr[51];
 
 struct BinaryTreeNode{
 	char value;
+	int num;
 	BinaryTreeNode *left;
 	BinaryTreeNode *right;
 };
 
 stack<BinaryTreeNode*> TreeStack;
 stack<char> OperatorStack;
+stack<int> Final;
 
 void Trans(char str[],char post[],int &m){
 	stack<char>Stack;
@@ -40,13 +44,24 @@ void Trans(char str[],char post[],int &m){
 		}
 		 if(str[i]=='-')
             {
-				if(!Stack.empty()&&(Stack.top()=='*'||Stack.top()=='/'))
+				if(!Stack.empty()&&(Stack.top()=='*'||Stack.top()=='/'||Stack.top()=='+'))
                 {
 					post[m++]=Stack.top();Stack.pop();Stack.push(str[i]);
 				}else{Stack.push(str[i]);}
             }
-		 if(str[i] == '*' || str[i] == '/'){
-			 Stack.push(str[i]);
+		 if(str[i] == '*')
+		 {
+			 if(!Stack.empty()&&(Stack.top()=='/')){
+				 post[m++]=Stack.top();Stack.pop();Stack.push(str[i]);
+			 }
+			 else Stack.push(str[i]);
+		 }
+		 if(str[i] == '/')
+		 {
+			 if(!Stack.empty()&&(Stack.top()=='*')){
+				 post[m++]=Stack.top();Stack.pop();Stack.push(str[i]);
+			 }
+			 else Stack.push(str[i]);
 		 }
 		 if(str[i] == ')'){
 			 while(Stack.top() != '('){
@@ -69,6 +84,7 @@ BinaryTreeNode *BuildTree(){
 			node->value = OutStr[i];
 			node->left = NULL;
 			node->right = NULL;
+			node->num = 0;
 			TreeStack.push(node);
 		}
 		else if (OutStr[i] == '*' || OutStr[i] == '/' || OutStr[i] == '+' || OutStr[i] == '-'){
@@ -76,6 +92,7 @@ BinaryTreeNode *BuildTree(){
 			temp->value = OutStr[i];
 			temp->left = NULL;
 			temp->right = NULL;
+			temp->num = 0;
 			temp->right = TreeStack.top();
 			TreeStack.pop();
 			temp->left = TreeStack.top();
@@ -124,15 +141,16 @@ void Delete_Out_Disp(){
 	}
 	for(int i = 0;i < effectiveLine;i++){
 		for(int k = 0;k < 100;k++){
-			if(dispbuf[i][j] != '#'){
-				cout << dispbuf[i][j];
+			if(dispbuf[i][k] != '#'){
+				cout << dispbuf[i][k];
 			}
-			else break;
+			else {
+				break;
+			}
 		}
 		cout << endl;
 	}
 }
-
 
 void PrintTree(BinaryTreeNode *root,int root_x,int root_y,int r_c_interval, char disp_buf[][100]){
 	//参数从左至右依次为：根结点、根结点的水平位置、根结点的竖直位置、当前根结点与其左右儿子的水平间距、用于暂存打印内容的缓存
@@ -153,12 +171,54 @@ void PrintTree(BinaryTreeNode *root,int root_x,int root_y,int r_c_interval, char
 	PrintTree(root->right,RightChild,(root_y + 2),(r_c_interval >> 1),disp_buf);
 }
 
+void GetNum(){
+	for(int i = 0;i < strlen(OutStr);i++){
+		if((OutStr[i] >= 'a' && OutStr[i] <= 'z') || (numSeq[i] >= 'A' && numSeq[i] <= 'Z')){
+			Final.push(numSeq[i]);
+		}
+		else{
+			int a,b;
+			int c;
+			a = Final.top();
+			Final.pop();
+			b = Final.top();
+			Final.pop();
+			switch(OutStr[i]){
+				case '+':
+					c = a+b;
+					break;
+				case '-':
+					c = b-a;
+					break;
+				case '*':
+					c = a*b;
+					break;
+				case '/':
+					c = b/a;
+					break;
+				default:;
+			}
+			Final.push(c);
+		}
+	}
+	result = Final.top();
+}
+
 int main(){
 	cin >> str;
 	cin >> variable;
 	n = 0;
-	int level;
+	int level,input;
+	char give;
 	Trans(str,OutStr,n);
+	while(variable--){
+		cin >> give >> input;
+		for(int i = 0;i < strlen(str);i++){
+			if(OutStr[i] == give){
+				numSeq[i] = input;
+			}
+		}
+	}
 	for(int j=0;j<n;j++){
 		cout << OutStr[j];
 	}
@@ -166,7 +226,10 @@ int main(){
 	BinaryTreeNode *root = BuildTree();
 	Init_Disp();
 	level = CalcuTreeLevel(root);
-	PrintTree(root,pow(2,level-1),pow(2,level-1),pow(2,level-2),dispbuf);
+	//cout << pow(2,level-1)-1;
+	PrintTree(root,pow(2,level-1),0,pow(2,level-2),dispbuf);
 	Delete_Out_Disp();
+	GetNum();
+	cout << result << endl;
 	return 0;
 }
