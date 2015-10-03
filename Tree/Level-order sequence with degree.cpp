4d142a926dbf32febdde84a3f5455b75d78a5add
hellozts4120 <14307110190@fudan.edu.cn>
2015-10-03 21:07:55 +0800

    补一周前的坑.

--- Tree/Level-order sequence with degree.cpp
+++ Tree/Level-order sequence with degree.cpp
@@ -3,79 +3,61 @@
 #include<queue>
 using namespace std;
 
-struct DegreeTree{
+struct Tree{
 	char info;
-	int degree;
-	DegreeTree *left;
-	DegreeTree *right;
-};
-DegreeTree *root[100];
-
-DegreeTree *MakeTree(DegreeTree *root){
-	DegreeTree *p = new DegreeTree;
-	p->left = NULL;
-	p->right = NULL;
-	char space,num,next;
-	cin >> root->info;
-	cin >> space;
-	cin >> num;
-	cin >> space;
-	root->degree = num - '0';
-	cin >> next;
-	while(next != '\n'){
-		root->left = p;
-		p->info = next;
-		cin >> space;
-		cin >> num;
-		cin >> space;
-		p->degree = num - '0';
-		(root->degree)--;
-		if(root->degree >= 0){
-			while(root->degree > 0){
-				DegreeTree *q = new DegreeTree;
-				q->left = NULL;
-				q->right = NULL;
-				cin >> q->info;
-				cin >> space;
-				cin >> num;
-				cin >> space;
-				q->degree = num - '0';
-				p->right = q;
-				p = q;
-				(root->degree)--;
-			}
+	Tree *child[26];
+	Tree(char name){
+		info = name;
+		for(int i = 0; i < 26; i++){
+			child[i] = NULL;
 		}
+	};
+	~Tree(){
+		for(int i = 0; i < 26; i++){
+			delete child[i];
+			child[i] = NULL;
+		}
+	};
+};
 
+void makeTree(Tree *&root){
+	int sonNum;
+	char name;
+	queue<Tree*>q_root;
+	queue<int>q_child_seq;
+	cin >> name >> sonNum;
+	root = new Tree(name);
+	for(int i = 0; i < sonNum; i++){
+		q_root.push(root);
+		q_child_seq.push(i);
+	}
+	while(!q_root.empty()){
+		Tree *curNode = q_root.front();
+		cin >> name >> sonNum;
+		curNode -> child[q_child_seq.front()] = new Tree(name);
+		for(int i = 0; i < sonNum; i++){
+			q_root.push(curNode -> child[q_child_seq.front()]);
+			q_child_seq.push(i);
+		}
+		q_root.pop();
+		q_child_seq.pop();
 	}
 }
 
-void PrintTree(DegreeTree *root){
-	if(root == NULL){
-		return;
+void PostOut(Tree *root){
+	for(int i = 0; (root->child[i])&&(i<26); i++){
+		PostOut(root->child[i]);
 	}
-	PrintTree(root->left);
-	cout << root->info << " ";
-	PrintTree(root->right);
-}
-
-void DeleteTree(DegreeTree *root){
-	if(root != NULL){
-		DeleteTree(root->left);
-		DeleteTree(root->right);
-		delete root;
-		root = NULL;	//∑¿÷π“∞÷∏’Î;
-	}
+	cout << root->info << ' ';
 }
 
 int main(){
 	int n;
 	cin >> n;
-	for(int i = 0;i < n;i++){
-		MakeTree(root[i]);
-	}
-	for(int i = 0;i < n;i++){
-		PrintTree(root[i]);
-		DeleteTree(root[i]);
+	Tree *root;
+	while(n--){
+		makeTree(root);
+		PostOut(root);
 	}
 	return 0;
 }
